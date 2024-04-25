92b2ebe1630c1a195b8d9b6f53d0d1ef2ebdf8f4
mudassiraa <alimudassir1a@gmail.com>
2024-04-25 13:13:08 +0500

    Adding a colourfull currency converter

---
+++ main.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+console.log(chalk.bgCyan.bold.italic("\n\t <<< Wellcome to CURRENCY coverter with me >>> \n"));
+
+let exchange_rate: any = {
+    "USD": 1,
+    "EUR": 0.9,
+    "JYP": 113,
+    "CAD": 1.3,
+    "AUD": 1.65,
+    "PKR": 278.01,
+}
+let user_answer = await inquirer.prompt([
+    {
+        name: "from_currency",
+        type: "list",
+        message: chalk.blueBright.italic("Select the currency to convert from:"),
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "to_currency",
+        type: "list",
+        message: chalk.blueBright.italic("Select the currency to convert into:"),
+        choices: ["USD", "EUR", "JYP", "CAD", "AUD", "PKR"]
+    },
+    {
+        name: "amount",
+        type: "input",
+        message:chalk.blueBright.italic("Enter the amount to convert:")
+    }
+]);
+let from_amount = exchange_rate[user_answer.from_currency];
+let to_amount = exchange_rate[user_answer.to_currency];
+let amount = user_answer.amount
+
+let base_amount = amount / from_amount
+let converted_amount = base_amount * to_amount
+console.log(`converted amount = ${chalk.greenBright.bold(converted_amount.toFixed(2))}`);
